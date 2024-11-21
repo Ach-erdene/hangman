4df048dfa548a533c37f59f6e793481f6f3589ad
Ach-erdene <141896884+Ach-erdene@users.noreply.github.com>
2024-11-21 19:25:22 +0800

    Update profile.js

--- modules/profile.js
+++ modules/profile.js
@@ -1,8 +1,8 @@
 import { uiController } from "./uiController.js";
 const ui = new uiController();
 
-const pathsHappy = ["../zurag/3.gif", "../zurag/4.png"];
-const pathAngry = ["../zurag/5.jpg", "../zurag/6.png", "../zurag/46-469365_clip-art-angry-stick-figure-meme-angry-meme.png"];
+const pathsHappy = ["https://ach-erdene.github.io/zurag/3.gif", "https://ach-erdene.github.io/zurag/4.png"];
+const pathAngry = ["https://ach-erdene.github.io/zurag/5.jpg", "https://ach-erdene.github.io/zurag/6.png", "https://ach-erdene.github.io/zurag/46-469365_clip-art-angry-stick-figure-meme-angry-meme.png"];
 
 
 export function profileNice(){
@@ -14,9 +14,9 @@ export function profileBad(){
 }
 
 export function ehlelProfile(){
-    ui.Dom.profile.src = "../zurag/ready.jpg";
+    ui.Dom.profile.src = "https://ach-erdene.github.io/zurag/ready.jpg";
 }
 
 export function dieProfile(){
-    ui.Dom.profile.src = "../zurag/dieee.png";
+    ui.Dom.profile.src = "https://ach-erdene.github.io/zurag/dieee.png";
 }
